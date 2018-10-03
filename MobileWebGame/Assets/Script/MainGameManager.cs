ae5715ed38f81cdb5b2dd1cabe219399dae6c466
muhaefath <32285392+muhaefath@users.noreply.github.com>
2018-10-03 21:45:34 +0700

    Week3Update3.1
    Singelton Game Manager Location

---
+++ MobileWebGame/Assets/Script/MainGameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainGameManager : MonoBehaviour {
+
+    public GameObject SelectLocation;
+
+    public int IndexLocations;
+
+    public MainGameManager instance;
+    public string[] ListOfScene;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void TurnOnSelectLocation(int IndexLocation) {
+        if (SelectLocation.activeSelf)
+        {
+            SelectLocation.SetActive(false);
+            IndexLocations = IndexLocation;
+            SceneManager.LoadScene(ListOfScene[IndexLocation -1]);
+        }
+        else {
+            SelectLocation.SetActive(true);
+        }
+    }
+}
